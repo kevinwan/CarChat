050e481a4f6ac81ad622a21241a95b3fe73221c0
zhaojia15 <jzhao@gongpingjia.com>
2015-01-09 14:14:46 +0800

    Initialization

---
+++ CarChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarChat
+//
+//  Created by Develop on 15/1/9.
+//  Copyright (c) 2015年 GongPingJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
